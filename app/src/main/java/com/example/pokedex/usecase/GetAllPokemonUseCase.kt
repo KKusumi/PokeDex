0b5bc89d9a11022e6cfced5535db0be90692b961
KKusumi <kusumi_katsumi@andfactory.co.jp>
2020-03-05 08:38:56 +0900

    [App] 全ポケモン情報取得UseCase実装

--- app/src/main/java/com/example/pokedex/usecase/GetAllPokemonUseCase.kt
+++ app/src/main/java/com/example/pokedex/usecase/GetAllPokemonUseCase.kt
@@ -2,7 +2,10 @@ package com.example.pokedex.usecase
 
 import com.example.pokedex.api.client.PokeApiClient
 import com.example.pokedex.api.response.PokemonListResponse
+import com.example.pokedex.model.PokeDexError
 import com.example.pokedex.model.Result
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
 
 interface GetAllPokemonUseCase {
     suspend fun execute(): Result<PokemonListResponse>
@@ -10,7 +13,23 @@ interface GetAllPokemonUseCase {
 
 class GetAllPokemonUseCaseImpl(private val pokeApiClient: PokeApiClient) : GetAllPokemonUseCase {
     override suspend fun execute(): Result<PokemonListResponse> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        kotlin.runCatching {
+            pokeApiClient.fetchAllPokemon()
+        }.fold(
+            onSuccess = {
+                return Result.Success(it)
+            },
+            onFailure = {
+                return when(it) {
+                    is SocketTimeoutException, is UnknownHostException -> {
+                        Result.Error(PokeDexError.NetworkError())
+                    }
+                    else -> {
+                        Result.Error(PokeDexError.UnDefined())
+                    }
+                }
+            }
+        )
     }
 }
 
