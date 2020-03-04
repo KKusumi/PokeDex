1fab300f34f7154bf7624d71d46dcd75baa47573
KKusumi <kusumi_katsumi@andfactory.co.jp>
2020-03-05 08:21:43 +0900

    [API] PokeDexErrorに未定義のエラーを追加

--- app/src/main/java/com/example/pokedex/model/PokeDexError.kt
+++ app/src/main/java/com/example/pokedex/model/PokeDexError.kt
@@ -3,10 +3,13 @@ package com.example.pokedex.model
 sealed class PokeDexError(
     val alertTitle: String = "エラーが発生しました。",
     val alertMessage: String = ""
-) : Throwable() {
+) : Exception() {
+
     /**
      * ここにエラーのパターンをクラスとして追加していく。
      * エラーごとにメッセージだけ変える。
      */
     class NetworkError : PokeDexError(alertMessage = "通信状況を確認し、再度お試しください。")
+
+    class UnDefined : PokeDexError(alertMessage = "")
 }
