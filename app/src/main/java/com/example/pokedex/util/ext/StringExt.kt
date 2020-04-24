de7ea3129a1745473094accd7100e54b465c1d3c
KKusumi <kusumi_katsumi@andfactory.co.jp>
2020-04-24 21:57:17 +0900

    [App] 拡張関数

--- app/src/main/java/com/example/pokedex/util/ext/StringExt.kt
+++ app/src/main/java/com/example/pokedex/util/ext/StringExt.kt
@@ -2,7 +2,7 @@ package com.example.pokedex.util.ext
 
 // PokemonListResponse.Pokemon.urlから図鑑ナンバーを生成する。
 fun generatePokemonNumber(url: String) =
-    url.removePrefix("https://pokeapi.co/api/v2/pokemon/").removeSuffix("/")
+    url.removePrefix("https://pokeapi.co/api/v2/pokemon/").removeSuffix("/").toInt()
 
 fun generatePokemonFrontImageUrl(number: String) =
     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png"
--- app/src/main/java/com/example/pokedex/util/ext/ViewExt.kt
+++ app/src/main/java/com/example/pokedex/util/ext/ViewExt.kt
@@ -1,5 +1,6 @@
 package com.example.pokedex.util.ext
 
+import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 
@@ -10,4 +11,9 @@ fun bindImageFromUrl(view: ImageView, imageFromUrl: String?) {
             .load(GlideUrlNoParams(imageFromUrl))
             .into(view)
     }
+}
+
+@BindingAdapter(value = ["visibleGone"])
+fun showHide(view: View, show: Boolean?) {
+    view.visibility = if (show == true) View.VISIBLE else View.GONE
 }
