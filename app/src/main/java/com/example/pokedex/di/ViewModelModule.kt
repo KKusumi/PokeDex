1d2a5924c2ac0077ae2ef0965aa6956ffc4e0095
KKusumi <kusumi_katsumi@andfactory.co.jp>
2020-04-24 21:49:21 +0900

    [PokemonDetail] ViewModelModuleにPokemonDetailViewModel追加

--- app/src/main/java/com/example/pokedex/di/ViewModelModule.kt
+++ app/src/main/java/com/example/pokedex/di/ViewModelModule.kt
@@ -1,10 +1,14 @@
 package com.example.pokedex.di
 
 import com.example.pokedex.ui.home.HomeViewModel
+import com.example.pokedex.ui.pokemondetail.PokemonDetailViewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
     factory {
         HomeViewModel(get(), get())
+    }
+    factory {
+        PokemonDetailViewModel(get(), get())
     }
 }
