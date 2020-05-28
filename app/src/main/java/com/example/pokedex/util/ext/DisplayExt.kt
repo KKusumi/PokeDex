7a1f516d203761fd540059ef2459f68ea22efa70
KKusumi <kusumi_katsumi@andfactory.co.jp>
2020-05-28 10:01:13 +0900

    [PokemonDetail] ステータスバーの長さ動的に変わるように修正

--- app/src/main/java/com/example/pokedex/util/ext/DisplayExt.kt
+++ app/src/main/java/com/example/pokedex/util/ext/DisplayExt.kt
@@ -2,10 +2,6 @@ package com.example.pokedex.util.ext
 
 import android.content.res.Resources
 
-/**
- * Created by watanabe on 2018/05/25.
- */
-
 fun Int.toDp(): Int = (this / Resources.getSystem().displayMetrics.density).toInt()
 
 fun Int.toPx(): Int = (this * Resources.getSystem().displayMetrics.density).toInt()
--- app/src/main/java/com/example/pokedex/util/ext/ViewExt.kt
+++ app/src/main/java/com/example/pokedex/util/ext/ViewExt.kt
@@ -67,3 +67,16 @@ fun ImageView.setTypeHalfCircle(typeName: String?) {
         )
     }
 }
+
+@BindingAdapter("status_bar_width")
+fun View.setStatusBarWidth(param: Int?) {
+    if (param != null) {
+        val layoutParams = this.layoutParams
+        /**
+         * 本当は長さの計算式これじゃないけど、
+         * デザイン日本語→実装英語なのでバーの長さいい感じに調整している。
+         */
+        layoutParams.width = ((128 * param) / 255) * 2
+        this.layoutParams = layoutParams
+    }
+}
