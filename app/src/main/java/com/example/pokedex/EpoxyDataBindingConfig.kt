1ecc49c571f83a7fe8b5be4fe5678947280b36cc
KKusumi <kusumi_katsumi@andfactory.co.jp>
2021-01-24 18:46:15 +0900

    [App] ライブラリを最新にした

---
+++ app/src/main/java/com/example/pokedex/EpoxyDataBindingConfig.kt
@@ -0,0 +1,6 @@
+package com.example.pokedex
+import com.airbnb.epoxy.EpoxyDataBindingPattern
+
+
+@EpoxyDataBindingPattern(rClass = R::class, layoutPrefix = "item")
+interface EpoxyDataBindingConfig
--- app/src/main/java/com/example/pokedex/package-info.java
+++
@@ -1,5 +0,0 @@
-@EpoxyDataBindingPattern(rClass = R.class, layoutPrefix = "item")
-
-package com.example.pokedex;
-
-import com.airbnb.epoxy.EpoxyDataBindingPattern;
